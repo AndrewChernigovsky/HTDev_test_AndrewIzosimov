45f132837a666be3016a11b56531a86c93b2a04e
Andrew Chernigovsky <chernigovsky108@gmail.com>
2021-12-26 19:18:32 +0300

    js

--- source/js/script.js
+++ source/js/script.js
@@ -1,1 +1,40 @@
-import { Slider } from "./sliderProjects";
+const sliderProjects = document.getElementById('siderProjects');
+const sliderContent = document.querySelectorAll('.slider-projects__content-wrapper');
+const sliderActive = document.querySelector('.slider-projects__content-wrapper--active');
+
+var indexSlider = 1;
+
+function nextSlider() {
+  ShowSlider(indexSlider += 1);
+}
+
+function previousSlider() {
+  ShowSlider(indexSlider -= 1);
+}
+
+function currentSlider(n) {
+  ShowSlider(n);
+}
+
+function ShowSlider(n) {
+
+  if (n > sliderContent.length) {
+    indexSlider = 1
+  }
+  if (n < 1) {
+    indexSlider = sliderContent.length
+  }
+
+  for (let slide of sliderContent) {
+    if (!slide.classList.contains('slider-projects__content-wrapper--active'))
+      slide.classList.add('slider-projects__content-wrapper--active');
+    else {
+      slide.classList.remove('slider-projects__content-wrapper--active');
+    }
+  }
+    sliderContent[indexSlider - 1].classList.remove('slider-projects__content-wrapper--active');
+}
+
+sliderContent.forEach(e => {
+  e.addEventListener('click', ShowSlider(indexSlider));
+});
--- source/js/sliderProjects.js
+++ source/js/sliderProjects.js
@@ -1,14 +1,15 @@
-export const Slider = () => {
-  const sliderProjects = document.getElementById('siderProjects');
-  const sliderContent = document.querySelectorAll('slider-projects__content-wrapper');
-  const sliderContentAcitve = document.querySelector('slider-projects__content-wrapper--active');
+// const sliderProjects = document.getElementById('siderProjects');
+// const sliderContent = document.querySelectorAll('slider-projects__content-wrapper');
+// const sliderContentAcitve = document.querySelector('slider-projects__content-wrapper--active');
 
+// const Slider = function() {
+//   sliderContent.forEach((e) => {
+//     if (e.contains('slider-projects__content-wrapper--active')) {
+//       e.classList.remove('slider-projects__content-wrapper--active')
+//     } else {
+//       e.classList.add('slider-projects__content-wrapper--active')
+//     }
+//   });
+// }
 
-  sliderContent.forEach(e => {
-    if(e.contains('slider-projects__content-wrapper--active')) {
-      e.classList.remove('slider-projects__content-wrapper--active')
-    } else {
-      e.classList.add('slider-projects__content-wrapper--active')
-    }
-  });
-}
+// export {sliderProjects, sliderContent, sliderContentAcitve, Slider};
